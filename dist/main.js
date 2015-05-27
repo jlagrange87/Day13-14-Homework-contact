85883a0b37ee98422d78d62a653cd3d70ce61191
Joshua Lagrange <jlagrange87@gmail.com>
2015-05-27 15:18:22 -0500

    Finished Hard mode with success window

--- dist/main.js
+++ dist/main.js
@@ -18,55 +18,51 @@ function start(e){
 
 	function validate(e){
 		var foundError = false;
-			var $nameError = ("#name-error");
-			var $emailError = ("#email-error");
-			var $websiteError = ("#website-error");
-			var $messageError = ("#message-error");
+		var $nameError = ("#name-error");
+		var $emailError = ("#email-error");
+		var $websiteError = ("#website-error");
+		var $messageError = ("#message-error");
 
-			if($name.val() == ""){
-				$("#name").siblings('#name-error').addClass('active-error').html("Name cannot be left empty");
-				foundError = true;
-			}
-			else {
-				$("#name").siblings("#name-error").removeClass("active-error");
-				foundError = false;
-			}
-
-			if($email.val() == ""){
-				$("#email").siblings('#email-error').addClass('active-error').html("Email cannot be left empty");
-				foundError = true;
-			}
-			else if($email.val().indexOf("@") == -1){
-				$("#email").siblings('#email-error').addClass('active-error').html("Email must contain an '@'");
-				foundError = true;
-			}
-			else {
-				$("#email").siblings("#email-error").removeClass("active-error");
-				foundError = false;
-			}
-
-			if($website.val() == ""){
-				$("#website").siblings('#website-error').addClass('active-error').html("Website cannot be left empty");
-				foundError = true;
-			}
-			else if($website.val().indexOf("http://") == -1){
-				$("#website").siblings('#website-error').addClass('active-error').html("Website must start with http://");
-				foundError = true;
-			}
-			else {
-				$("#website").siblings("#website-error").removeClass("active-error");
-				foundError = false;
-			}
-
-			if($message.val() == ""){
-				$("#message").siblings('#message-error').addClass('active-error').html("Message cannot be left empty");
-				foundError = true;
-			}
-			else {
-				$("#message").siblings("#message-error").removeClass("active-error");
-				foundError = false;
-			}
-
+		if($name.val() == ""){
+			$("#name").siblings('#name-error').addClass('active-error').html("Name cannot be left empty");
+			foundError = true;
+		}
+		else {
+			$("#name").siblings("#name-error").removeClass("active-error");
+			foundError = false;
+		}
+		if($email.val() == ""){
+			$("#email").siblings('#email-error').addClass('active-error').html("Email cannot be left empty");
+			foundError = true;
+		}
+		else if($email.val().indexOf("@") == -1){
+			$("#email").siblings('#email-error').addClass('active-error').html("Email must contain an '@'");
+			foundError = true;
+		}
+		else {
+			$("#email").siblings("#email-error").removeClass("active-error");
+			foundError = false;
+		}
+		if($website.val() == ""){
+			$("#website").siblings('#website-error').addClass('active-error').html("Website cannot be left empty");
+			foundError = true;
+		}
+		else if($website.val().indexOf("http://") == -1){
+			$("#website").siblings('#website-error').addClass('active-error').html("Website must start with http://");
+			foundError = true;
+		}
+		else {
+			$("#website").siblings("#website-error").removeClass("active-error");
+			foundError = false;
+		}
+		if($message.val() == ""){
+			$("#message").siblings('#message-error').addClass('active-error').html("Message cannot be left empty");
+			foundError = true;
+		}
+		else {
+			$("#message").siblings("#message-error").removeClass("active-error");
+			foundError = false;
+		}
 		if(foundError === false){
 			$("form").css("display", "none");
 			$("div").addClass("success");
